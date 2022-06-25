09813d95c8fcd9d826e5dd6c1a3bca8a89d01675
unknown <tabassumshakira999@gmail.com>
2022-06-26 03:44:00 +0600

    fourth commit

---
+++ problem8.py
@@ -0,0 +1,11 @@
+fruitOne=input('enter fruit name')
+fruitTwo=input('enter fruit name')
+fruitThree=input('enter fruit name')
+fruitFour=input('enter fruit name')
+fruitFive=input('enter fruit name')
+fruitSix=input('enter fruit name')
+fruitSeven=input('enter fruit name')
+fruitEight=input('enter fruit name')
+
+allFruit=[fruitOne,fruitTwo,fruitThree,fruitFour,fruitFive,fruitSix,fruitSeven,fruitEight]
+print(allFruit)
---
+++ problem9.py
@@ -0,0 +1,12 @@
+markOne=int(input("enter mark"))
+markTwo=int(input("enter mark"))
+markThree=int(input("enter mark"))
+markFour=int(input("enter mark"))
+markFive=int(input("enter mark"))
+markSix=int(input("enter mark"))
+markSeven=int(input("enter mark"))
+markEight=int(input("enter mark"))
+
+allMark=[markOne,markTwo,markThree,markFour,markFive,markSix,markSeven,markEight]
+allMark.reverse()
+print(allMark)

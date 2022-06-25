aa50b1c98b4edfdf5e216a192de5916636b42699
unknown <tabassumshakira999@gmail.com>
2022-06-26 03:28:14 +0600

    third commit

---
+++ list.py
@@ -0,0 +1,15 @@
+# lists =[1,2,3,4,5,6]
+# print(lists[0])
+# lists[2]=45
+# print(lists)
+arrayList=[23,45,68,98,65,78,45,67];
+# arrayList.sort()
+# print(arrayList)
+arrayList.reverse()
+print(arrayList)
+arrayList.append(23)
+print(arrayList)
+arrayList.insert(2,123)
+print(arrayList)
+# tauple is immutable, you can not change value or intert varlue like list
+ 
